32bdc2ee2aa90ca95a5ef0194442b47159edad3a
Adam Waldron <acw86@uw.edu>
2017-04-11 12:52:32 -0700

    fire button works.

--- app/src/main/java/tcss450/uw/edu/dialogs/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/dialogs/MainActivity.java
@@ -1,7 +1,9 @@
 package tcss450.uw.edu.dialogs;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launch(View v) {
+        DialogFragment fragment = null;
+        if (v.getId() == R.id.fire_missiles_button) {
+            fragment = new FireMissilesDialogFragment();
+        } if (fragment != null)
+            fragment.show(getSupportFragmentManager(), "launch");
+    }
+
 }
