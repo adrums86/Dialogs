a4fc1581dce412285d769c22d1c3008100e480e2
Adam Waldron <acw86@uw.edu>
2017-04-11 09:44:18 -0700

    wtf mate.

--- app/src/main/java/tcss450/uw/edu/dialogs/NewActivity.java
+++ app/src/main/java/tcss450/uw/edu/dialogs/NewActivity.java
@@ -10,6 +10,5 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
-        Log.wtf("NewActivity", "AHHHH!");
     }
 }
